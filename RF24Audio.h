77791e23d33e8baa7754317fc1ff9b47c672f818
Brendan <2bndy5@gmail.com>
2021-03-29 20:02:49 -0700

    code fmt; abstract doc main pg

--- RF24Audio.h
+++ RF24Audio.h
@@ -1,19 +1,18 @@
-
-/* RFAudio Library: by TMRh20 2011-2014*/
-
-#ifndef __RF24Audio_H__
-#define __RF24Audio_H__
-
 /**
  * @file RF24Audio.h
  *
  * Class declaration for RF24Audio Library
+ *
+ * RFAudio Library: by TMRh20 2011-2014
  */
+
+#ifndef __RF24Audio_H__
+#define __RF24Audio_H__
 
 class RF24;
 
 /**
- * TMRh20 2014 - RF24Audio: Arduino Realtime Audio Streaming library
+ * @brief Arduino Realtime Audio Streaming library
  *
  * This class implements an Audio Streaming library using nRF24L01(+) radios driven
  * by the [Optimized RF24 library](https://github.com/TMRh20/RF24).
@@ -23,103 +22,103 @@ class RF24Audio
 {
 
 public:
-   /**
-    * Setup the radio and radio identifier
-    * @note Changing radioNum is only required if utilizing private node-to-node communication as
-    * opposed to broadcasting to the entire radio group
-    *
-    * @code
-    *	RF24 radio(48, 49);          // Initialize the radio driver
-    *	RF24Audio rfAudio(radio, 0); // Initialize the audio driver
-    * @endcode
-    *
-    * @param _radio   The underlying radio driver instance
-    * @param radioNum The radio identifier
-    */
+    /**
+     * Setup the radio and radio identifier
+     * @note Changing radioNum is only required if utilizing private node-to-node communication as
+     * opposed to broadcasting to the entire radio group
+     *
+     * @code
+     *	RF24 radio(48, 49);          // Initialize the radio driver
+     *	RF24Audio rfAudio(radio, 0); // Initialize the audio driver
+     * @endcode
+     *
+     * @param _radio   The underlying radio driver instance
+     * @param radioNum The radio identifier
+     */
 	RF24Audio(RF24& _radio, byte radioNum);
 
-   /**
-    * Initialize the radio and audio library
-    *
-    * Generally called in setup to initialize the radio
-    * @code
-    *	rfAudio.begin();
-    * @endcode
-    */
+    /**
+     * Initialize the radio and audio library
+     *
+     * Generally called in setup to initialize the radio
+     * @code
+     *	rfAudio.begin();
+     * @endcode
+     */
 	void begin();
 
-   /**
-    * Volume Control
-    * @code
-    *	rfAudio.volume(1); // Raise the volume
-    * @endcode
-    * @param upDn Set 0 to lower volume, 1 to raise volume
-    *
-    */
+    /**
+     * Volume Control
+     * @code
+     * rfAudio.volume(1); // Raise the volume
+     * @endcode
+     * @param upDn Set 0 to lower volume, 1 to raise volume
+     *
+     */
 	void volume(bool upDn);
 
-   /**
-    * Volume Control
-    * @code
-    *	rfAudio.setVolume(4); // Set the volume to mid-level
-    * @endcode
-    * @param vol Set at 0 to 7 for range of volume control
-    *
-    */
+    /**
+     * Volume Control
+     * @code
+     * rfAudio.setVolume(4); // Set the volume to mid-level
+     * @endcode
+     * @param vol Set at 0 to 7 for range of volume control
+     *
+     */
 	void setVolume(char vol);
 
-   /**
-    * Control transmission through code
-    * @code
-    *	rfAudio.transmit(); // Begin realtime audio streaming
-    * @endcode
-    * Call this function to begin transmission
-    *
-    */
+    /**
+     * Control transmission through code
+     * @code
+     * rfAudio.transmit(); // Begin realtime audio streaming
+     * @endcode
+     * Call this function to begin transmission
+     *
+     */
 	void transmit();
 
-   /**
-    * Stop transmission through code
-    * @code
-    *	rfAudio.receive(); // Stop audio streaming
-    * @endcode
-    * Call this function to stop transmission
-    *
-    */
+    /**
+     * Stop transmission through code
+     * @code
+     *	rfAudio.receive(); // Stop audio streaming
+     * @endcode
+     * Call this function to stop transmission
+     *
+     */
 	void receive();
 
-   /**
-    * Control of Private or Public Communication
-    *
-    * Call this function to establish private communication between nodes
-    * in a radio group, or to switch back to public transmission.
-    * @note Using a radioID of 255 will disable private communication and broadcast to all nodes
-    *
-    * @code
-    *	rfAudio.broadcast(1); 	// Only transmit audio to radio number 1
-    *	rfAudio.broadcast(255);  // Transmit audio to all radios in the group
-    * @endcode
-    * @param radioID  Set the radioID of the radio to communicate privately with.
-    */
+    /**
+     * Control of Private or Public Communication
+     *
+     * Call this function to establish private communication between nodes
+     * in a radio group, or to switch back to public transmission.
+     * @note Using a radioID of 255 will disable private communication and broadcast to all nodes
+     *
+     * @code
+     * rfAudio.broadcast(1); 	// Only transmit audio to radio number 1
+     * rfAudio.broadcast(255);  // Transmit audio to all radios in the group
+     * @endcode
+     * @param radioID  Set the radioID of the radio to communicate privately with.
+     */
 	void broadcast(byte radioID);
 
-   /**
-    * Get any of the preset radio addresses
-    *
-    * Useful for listening nodes who wish to create private or additional radio groups
-    * The library has 14 predefined radio addreses. All radios listen/write on the first
-    * two addresses (0, 1), and engage a private channel based on the radio number.
-    * Radio 0 listens on address 2, Radio 1 on address 3, etc.
-    *
-    * @code
-    *	uint64_t newAddress = rfAudio.getAddress(3);	// Gets the 3rd defined radio address
-    *    OR
-    *	radio.openReadingPipe(0, rfAudio.getAddress(7)); // Listens on the 7th defined radio address
-    * @endcode
-    * @param addressNo  Numbers 0 through 14 to access any part of the defined address array
-    * @return RadioAddress:  Returns the requested predefined radio address
-    */
-   uint64_t getAddress(byte addressNo);
+    /**
+     * Get any of the preset radio addresses
+     *
+     * Useful for listening nodes who wish to create private or additional radio groups
+     * The library has 14 predefined radio addreses. All radios listen/write on the first
+     * two addresses (0, 1), and engage a private channel based on the radio number.
+     * Radio 0 listens on address 2, Radio 1 on address 3, etc.
+     *
+     * @code
+     * uint64_t newAddress = rfAudio.getAddress(3);	// Gets the 3rd defined radio address
+     *  OR
+     * radio.openReadingPipe(0, rfAudio.getAddress(7)); // Listens on the 7th defined radio address
+     * @endcode
+     * @param addressNo  Numbers 0 through 14 to access any part of the defined address array
+     * @return RadioAddress:  Returns the requested predefined radio address
+     */
+    uint64_t getAddress(byte addressNo);
 
 
 private:
@@ -141,14 +140,12 @@ void TX();
  */
 void RX();
 
-#endif
+#endif // __RF24Audio_H__
 
 
 /**
  * @example GettingStarted.ino
- *
  * This sketch is intended to demonstrate the basic functionality of the audio library.
- *
  */
 
 /**
@@ -159,100 +156,20 @@ void RX();
 /**
  * @example PrivateChannels.ino
  * This sketch is demonstrates the use of private channels (node-to-node) in a multi-radio group.
- *
  */
 
 /**
  * @example PrivateGroups.ino
  * This sketch is demonstrates the use of private groups (node-to-custom_groups) in a multi-radio group.
- *
  */
 
 /**
  * @example USB_Audio.ino
  * This sketch is demonstrates how to interact with the audio library directly using the core
  * RF24 radio library: http://nRF24.github.io/RF24/
- *
  */
 
 /**
- * @mainpage RF24Audio - Realtime Audio Streaming Library for Arduino
- *
- * This class implements a realtime audio streaming solution using nRF24L01(+) radios driven
- * by the newly [optimized RF24 library fork](http://nRF24.github.com/RF24/).
- *
- * @section Features Features
- * - <b>User friendly setup and configuration:</b> For beginners too: Just connect a radio module, microphone, and speaker. The library handles the rest.
- * - <b>Recording and broadcasting of audio</b> to multiple devices using only Arduino, RF24 modules and input/output (speaker/microphone) devices
- * - <b>Multicast:</b> Enables broadcasting to all nodes, single nodes, or partial groups of nodes
- * - <b>External controls:</b> Use external buttons or code to control audio streaming
- * - <b>Volume control:</b> Use external buttons or code to control audio volume on receiving devices.
- * - <b>Remote control:</b> Start recording remotely via radio commands (Currently cannot stop remote recording)
- * - <b>LED Indicator/Visualization:</b> Indicates audio playback and amplitude.
- * - <b>Customization:</b> Using the underlying RF24 core library allows custom interaction with audio devices running this library. Receive audio data
- *   and stream it to a PC over USB, create and broadcast computer generated audio in realtime, and more! See the [USB_Audio example](USB_Audio_8ino-example.html) for more info.
- * - <b>Create additional node groups:</b> Allows nodes to join private broadcast groups, and multicast only within their group as desired. See advanced section below.
- *
- * @section More How to learn more
- *
- * - [RF24Audio Library Class Documentation](classRF24Audio.html)
- * - [RF24: Underlying radio driver (2014 - Newly Optimized)](http://nRF24.github.io/RF24/)
- * - [My Blog: RF24 Optimization Overview](http://tmrh20.blogspot.com/2014/03/high-speed-data-transfers-and-wireless.html)
- * - [My Blog: RF24 Wireless Audio](http://tmrh20.blogspot.com/2014/03/arduino-radiointercomwireless-audio.html)
- * - [Download Current RF24Audio Driver](https://github.com/TMRh20/RF24Audio/archive/master.zip)
- * - [Download Optimized RF24 Radio Driver](https://github.com/TMRh20/RF24/archive/master.zip)
- * - [Newly Optimized RF24Network Class](http://nRF24.github.io/RF24Network/)
- *
- * @section Config Configuration and Setup
- *
- * The settings for the library will generally be detected automatically. To change from the default pin assignments etc, edit the
- * userConfig.h file. The mandatory user options in the configuration file are shown below:
- * @code
- * #define SAMPLE_RATE 24000   // The sample rate to use for transferring audio samples  Note: 44khz+ sample rate requires 8-bits per sample
- * #define RF_SPEED RF24_1MBPS // RF24_250KBPS will do 13-20khz+ sample rate, RF24_1MBPS up to 24-44khz+, RF24_2MBPS for higher. These are not limits, just a guide.
- * #define ANALOG_PIN A0       // The pin that analog readings will be taken from (microphone pin)
- * @endcode
- *
- * <b>Pin Assignments: See [the Setup page](Setup.html) for wiring diagrams</b>
- * - <b>Speakers:</b> Arduino Uno,Nano,etc: pins 9, 10   Arduino Mega: 11, 12  (Timer pins cannot be changed, but can use 1 pin and ground)
- * - <b>pin A0:</b> Microphone/Input pin
- * - <b>pin A1:</b> Transmission/Recording Start pin
- * - <b>pin A2:</b> Volume Up
- * - <b>pin A3:</b> Volume Down
- * - <b>pin A4:</b> Trigger remote recording (Only working with dual devices)
- * - <b>Cannot be changed:</b> LED Pin: Uno,Nano,etc: pin 6  Mega 2560: pin 13 (main LED pin)
- *
- *
- * See http://arduino.cc/en/Tutorial/InputPullupSerial for info on how to wire the buttons to the pins
- * See userConfig.h to change default pin assignments and options.
- *
- * @section Advanced Advanced Usage
- *
- * The radio pipes are defined as follows. For more complex multicast scenarios, radios can listen on any combination of pipes. <br>
- * Use the getAddress(); function to access the address array.<br>
- * @code
- * const uint64_t pipes[14] = {0xABCDABCD71LL,
- *                             0x544d52687CLL,
- *                             0x544d526832LL,
- *                             0x544d52683CLL,
- *                             0x544d526846LL,
- *                             0x544d526850LL,
- *                             0x544d52685ALL,
- *                             0x544d526820LL,
- *                             0x544d52686ELL,
- *                             0x544d52684BLL,
- *                             0x544d526841LL,
- *                             0x544d526855LL,
- *                             0x544d52685FLL,
- *                             0x544d526869LL
- *                            };
- * @endcode
- * By default, all radios will open the same reading & writing pipes on the first two addresses.<br>
- * Any radio that wishes to transmit, will reverse the addresses on the first two pipes, and begin to transmit to all the other nodes. <br>
- * Every radio will automatically be assigned the first two addresses, then one of the remaining addresses as a private channel, based on its radio number: <br> (0 = pipes[2], 1 = pipes[3]) <br>
- *
- * Additional addresses can be added by modifying the address array listed in userConfig.h
- *
  * @page Setup Boards & Wiring
  * @section Board Wiring
  * This page displays different options for wiring/board configuration.
@@ -264,5 +181,4 @@ void RX();
  * @image html "images/RF24AudioBasic_SmallAntenna.jpg" height=45% width=45%
  * Wiring diagram for SD streaming/multicast using TMRpcm library:
  * @image html "images/RF24Audio_FullSD.jpg" height=65% width=65%
- *
  */
--- userConfig.h
+++ userConfig.h
@@ -1,13 +1,31 @@
-
+/**
+ * @file userConfig.h
+ *
+ * macros for customization of RF24Audio
+ */
 
 /************  MANDATORY User Variables  ************/
+/**
+ * @brief The sample rate to use for transferring audio samples
+ *
+ * Maximum Range: Sample rate 16000, RF_SPEED RF24_250KBPS <br>
+ * Maximum Quality: Sample rate 44000, RF_SPEED RF24_2MBPS
+ * @note 44khz+ sample rate requires 8-bits per sample
+ */
+#define SAMPLE_RATE 24000
 
-// Maximum Range: Sample rate 16000, RF_SPEED RF24_250KBPS
-// Maximum Quality: Sample rate 44000, RF_SPEED RF24_2MBPS
+/**
+ * @brief configure the RF data rate
+ *
+ * These are not limits, just a guide:
+ * - RF24_250KBPS will do 13-20khz+ sample rate
+ * - RF24_1MBPS up to 24-44khz+
+ * - RF24_2MBPS for higher.
+ */
+#define RF_SPEED RF24_1MBPS
 
-#define SAMPLE_RATE 24000   // The sample rate to use for transferring audio samples  Note: 44khz+ sample rate requires 8-bits per sample
-#define RF_SPEED RF24_1MBPS // RF24_250KBPS will do 13-20khz+ sample rate, RF24_1MBPS up to 24-44khz+, RF24_2MBPS for higher. These are not limits, just a guide.
-#define ANALOG_PIN A0       // The pin that analog readings will be taken from (microphone pin)
+/** @brief The pin that analog readings will be taken from (microphone pin) */
+#define ANALOG_PIN A0
 
 /************  OverRides  ************/
 
@@ -17,17 +35,36 @@
 
 //#define speakerPin 9 // If using a non-standard board, override the timer1 pins
 //#define speakerPin 10
-#define ENABLE_LED     // Indicator pin. Using pin 6 on Uno enables audio visualization. Pin 13 on Mega 2560 (TIMER0 COMPA) The pin# cannot be changed.
+
+/**
+ * @brief Indicator pin
+ *
+ * Using pin 6 on Uno enables audio visualization. Pin 13 on Mega 2560 (TIMER0 COMPA)
+ * @note The pin number cannot be changed.
+ */
+#define ENABLE_LED
 
 /************  Optional/Advanced User Variables  ************/
 
 //#define MANUAL_BUTTON_HANDLING  // Disables button handling via timer0. Allow users to customize button handling
-#define TX_PIN A1                 // Button pin to trigger recording & transmission
-#define VOL_UP_PIN A2             // Pin for external volume control
-#define VOL_DN_PIN A3             // Pin for external volume control
-#define REMOTE_TX_PIN A4          // Pin for externally triggering remote recording
-#define REMOTE_RX_PIN 4           // Pin for externally stopping remote recording (needs timeout enabled)
-#define buffSize 32               // The size of the memory buffer to use. Not really configurable.
+
+/** Button pin to trigger recording & transmission */
+#define TX_PIN A1
+
+/** Pin for external volume control */
+#define VOL_UP_PIN A2
+
+/** Pin for external volume control */
+#define VOL_DN_PIN A3
+
+/** Pin for externally triggering remote recording */
+#define REMOTE_TX_PIN A4
+
+/** Pin for externally stopping remote recording (needs timeout enabled) */
+#define REMOTE_RX_PIN 4
+
+/** The size of the memory buffer to use. Not really configurable (set to maximum by default). */
+#define buffSize 32
 
 //#define speakerTX               // Whether to output to speaker while transmitting
 //#define oversampling            // Oversampling is recommended for low sample rates only. This only affects playback.
@@ -38,61 +75,56 @@
 /************  Automated pin selections, override by defining above  ************/
 
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || (__AVR_ATmega32U4__) || (__AVR_AT90USB646__) || defined(__AVR_AT90USB1286__) || (__AVR_ATmega128__) ||defined(__AVR_ATmega1281__)||defined(__AVR_ATmega2561__)
-  #define rampMega
+    #define rampMega
 
-  #if !defined (speakerPin)	// Speaker pin selection for mega etc
-    #define speakerPin 11   // The pins to output audio on. (11,12 on Mega 2560)
-  #endif
-  #if !defined (speakerPin2)
-    #define speakerPin2 12
-  #endif
-  #if defined (ENABLE_LED)
-    #define ledPin 13
-  #endif
+    // Speaker pin selection for mega etc
+    #if !defined (speakerPin)
+        #define speakerPin 11   // The pins to output audio on. (11,12 on Mega 2560)
+    #endif
 
-#else  //Speaker selection for Uno,Nano, etc
+    #if !defined (speakerPin2)
+        #define speakerPin2 12
+    #endif
 
-  #if !defined (speakerPin)
-  	#define speakerPin 9     // The pin used to output audio on UNO
-  #endif
-  #if !defined (speakerPin2)
-	#define speakerPin2 10     // The pin used to output audio on Nano
-  #endif
+    #if defined (ENABLE_LED)
+        #define ledPin 13
+    #endif
 
-  #if defined (ENABLE_LED)
-    #define ledPin 6
-  #endif
+#else
+
+    //Speaker selection for Uno,Nano, etc
+    #if !defined (speakerPin)
+        /** The pin used to output audio on UNO */
+        #define speakerPin 9
+    #endif
+
+    #if !defined (speakerPin2)
+        /** The pin used to output audio on Nano */
+        #define speakerPin2 10
+    #endif
+
+    #if defined (ENABLE_LED)
+        #define ledPin 6
+    #endif
 #endif
 
 
 //********Radio Defines ****************************
-// Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[14] = {0xABCDABCD71LL,
-                            0x544d52687CLL,
-                            0x544d526832LL,
-                            0x544d52683CLL,
-                            0x544d526846LL,
-                            0x544d526850LL,
-                            0x544d52685ALL,
-                            0x544d526820LL,
-                            0x544d52686ELL,
-                            0x544d52684BLL,
-                            0x544d526841LL,
-                            0x544d526855LL,
-                            0x544d52685FLL,
-                            0x544d526869LL
-                           };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/** Radio pipe addresses for the 2 nodes to communicate. */
+const uint64_t pipes[14] =
+{
+    0xABCDABCD71LL,
+    0x544d52687CLL,
+    0x544d526832LL,
+    0x544d52683CLL,
+    0x544d526846LL,
+    0x544d526850LL,
+    0x544d52685ALL,
+    0x544d526820LL,
+    0x544d52686ELL,
+    0x544d52684BLL,
+    0x544d526841LL,
+    0x544d526855LL,
+    0x544d52685FLL,
+    0x544d526869LL
+};
